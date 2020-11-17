964900b4a4f0b7482a3975c9e13472b6d90f7c47
Nikita Biryuchkov <55081007+biryuc@users.noreply.github.com>
2020-11-17 16:28:23 +0700

    Create nine.c

---
+++ nine.c
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <wait.h>
+//#include <sys/wait.h>
+#include <errno.h>
+
+// положительное значение pid - родительский процесс, 0 - порожденный процесс. -1 - ошибка. Оба процесса имеют одинаковую возможность получить управление. Поэтому любой из них может начать исполнение первым
+
+int main(int argc, char *argv[]){
+
+    pid_t pid;
+    int status;
+    char* arg0;
+    if ((pid = fork()) > 0) {
+        printf("Hello, how are you?\n");
+        if( wait(&status) == -1)
+        {
+            if(errno == EINTR) wait(&status);
+            perror("no incomplete subprocess!");
+            exit(1);
+        }
+        printf(" waited, goodbuy!");
+    }
+    else if (pid == 0){
+        arg0 = argv[0];
+        argv[0] = "cat";
+        execvp("cat", argv);
+        argv[0] = arg0;
+        perror("Exec error");
+        exit(1);
+    }
+    else {
+        perror(argv[0]);
+        exit(1);
+    }
+    exit(0);
+}
