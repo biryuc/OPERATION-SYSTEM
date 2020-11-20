ad0713b0e1ae9cfb3fdce873ee37fbe6a0ce6d70
Nikita Biryuchkov <55081007+biryuc@users.noreply.github.com>
2020-11-20 10:49:39 +0700

    Update Six.c

--- Six.c
+++ Six.c
@@ -45,7 +45,7 @@ int main(int argc, char* argv[])
     int fd, selectResult;
     int line_number = 0;
     int need_line = 1; 
-
+    int max_fd = 0;
 
     fd_set terminal;
 	struct timeval timeout;
@@ -86,7 +86,7 @@ int main(int argc, char* argv[])
         timeout.tv_sec = 5;
         timeout.tv_usec = 0;
 
-        selectResult = select(4, &terminal, NULL, NULL, &timeout);
+        selectResult = select(max_fd+1, &terminal, NULL, NULL, &timeout);
 
         if (selectResult  == -1) { 
             perror("select()");
